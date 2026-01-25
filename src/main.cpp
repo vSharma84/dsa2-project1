0dcc2ace669762a5058676b925dc209cc151a0b5
vSharma84 <vs84@students.uwf.edu>
2026-01-25 01:03:08 +0000

    Read userids from txt file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 int main() {
+    ifstream infile("names.txt");
+
+    if (!infile.is_open()) {
+        cout << "Could not open names.txt\n";
+        return 1;
+    }
+
+    vector<string> userids;
+    string userid;
+    string restOfLine;
+
+    while (infile >> userid) {
+        userids.push_back(userid);
+        getline(infile, restOfLine);
+    }
+
+    infile.close();
+
+    cout << "Read " << userids.size() << " userids\n";
+    cout << "First 5 userids:\n";
+
+    for (size_t i = 0; i < userids.size() && i < 5; ++i) {
+        cout << userids[i] << "\n";
+    }
+
     return 0;
 }
