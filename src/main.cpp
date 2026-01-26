5a651d40a9b3546651acda0667c3ecbbb8e36d57
vSharma84 <vs84@students.uwf.edu>
2026-01-26 01:45:02 +0000

    Added cipher encryption

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,30 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
+string encryptPassword(string password) {
+    string key = "jones";
+    string result = "";
+    int counter = 0;
+
+    for (int i = 0; i < password.size(); i++) {
+        int shift = (password[i] - 'a') + (key[counter] - 'a');
+        char encryptedChar = 'a' + (shift % 26);
+        
+        result += encryptedChar;
+
+        counter++;
+        if (counter >= key.size()) {
+            counter = 0;
+        }
+    }
+
+    return result;
+}
 
 int main() {
     ifstream infile("names.txt");
@@ -24,12 +46,30 @@ int main() {
 
     infile.close();
 
-    cout << "Read " << userids.size() << " userids\n";
-    cout << "First 5 userids:\n";
+    srand(time(0));
 
-    for (size_t i = 0; i < userids.size() && i < 5; ++i) {
-        cout << userids[i] << "\n";
+    ofstream outfile("rawdata.txt");
+
+    if (!outfile.is_open()) {
+        cout << "Could not create rawdata.txt\n";
+        return 1;
     }
 
+    for (int i = 0; i < userids.size(); i++) {
+        string password;
+
+        for (int j = 0; j < 9; j++) {
+            char c = 'a' + (rand() % 26);
+            password += c;
+        }
+
+        outfile << userids[i] << " " << password << "\n";
+    }
+
+    outfile.close();
+
+    cout << "Created rawdata.txt with " << userids.size() << " entries\n";
+
     return 0;
+
 }
